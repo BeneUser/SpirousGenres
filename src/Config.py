52ba8865a882568a40519b41809b3270b993c453
beneuser <baumgarten.benedikt@icloud.com>
2025-11-07 19:13:33 +0100

    Integrated (new) hyper params into config and training code

--- src/Config.py
+++ src/Config.py
@@ -1,10 +1,11 @@
 class Config:
-    def __init__(self, audio_dir_path, num_genres, train_part_size, val_part_size, test_part_size, batch_size, epochs, seed):
+    def __init__(self, audio_dir_path, num_genres, train_part_size, val_part_size, test_part_size, batch_size, learning_rate, epochs, seed):
         self.audio_dir_path = audio_dir_path
         self.num_genres = num_genres
         self.train_part_size = train_part_size
         self.val_part_size = val_part_size
         self.test_part_size = test_part_size
         self.batch_size = batch_size
+        self.learning_rate = learning_rate
         self.epochs = epochs
         self.seed = seed
