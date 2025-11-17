67ebf86dd0435b5715a142809bcc28c38dc7b471
Erik Schwaar <erik.schwaar@stud.uni-goettingen.de>
2025-11-17 17:56:06 +0000

    augmentation functions implementiert

--- src/Augmentations.py
+++ src/Augmentations.py
@@ -4,7 +4,7 @@ import torch
 
 
 class Augmentations():
-    def __init__(self, available_augmentations, num_augments, always_augment):
+    def __init__(self, available_augmentations, num_augments, always_augment, cropt_size, sampling_rate):
         #In principal, all of these augmentations may be used
         self.available_augmentations = list(self.__getattribute__(a) for a in available_augmentations)
 
@@ -16,6 +16,8 @@ class Augmentations():
         assert num_augments <= len(self.available_augmentations), "num_augments larger than the number of available augmentations!"
         self.num_augments = num_augments #Number of total augmentations (including those fixed in always_augment)
         self.fixed_augments = always_augment #Which augmentations to always use
+        self.cropt_size = cropt_size
+        self.sampling_rate = sampling_rate
 
         #Other local vars, eg such need for certain augmentations to work.
         #...
@@ -27,6 +29,45 @@ class Augmentations():
 
     def zero(self, audio_samples):
         return torch.zeros(audio_samples.size(0))
+
+    def RandomStartCrop(self, audio_samples):
+
+        length = audio_samples.size(0)
+        crop_len = self.cropt_size * self.sampling_rate
+
+        if length <= crop_len:
+            if length < crop_len:
+                pad_len = crop_len - length
+                audio_samples = torch.nn.functional.pad(audio_samples, (0, pad_len))
+            return audio_samples
+
+        start = torch.randint(0, length - crop_len + 1, (1,)).item()
+        end = start + crop_len
+        self.last_start = start
+        self.last_end = end
+        return audio_samples[start:end]
+
+    def RandomNoise(self, audio_samples):
+
+        min_factor = 0.0
+        max_factor = 0.05
+
+        factor = random.uniform(min_factor, max_factor)
+
+        std = audio_samples.std()
+        if std == 0:
+            std = 1
+
+        noise = torch.randn_like(audio_samples) * std * factor
+        return audio_samples + noise
+
+
+    def FlipWave(self, audio_samples):
+        return -audio_samples
+
+
+
+
 
 
     def augment(self, x):
@@ -49,8 +90,8 @@ class Augmentations():
             #Find out the actual position p
             pseudo_p = 0
             p = 0
-            while(pseudo_p != chosen or self.available_augmentations[p] == 1):
-                if self.available_augmentations[p] == 0:
+            while(pseudo_p != chosen or do_augments[p] == 1):
+                if do_augments[p] == 0:
                     pseudo_p += 1 #Only skip if the augmentation is not used yet.
                 p += 1
             #Set the augmentation to true
